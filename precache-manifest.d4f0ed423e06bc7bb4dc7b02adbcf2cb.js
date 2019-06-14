cd30b417632ae9a0b607b4b7d40ba372696c59af
Agasthian <agasthian_r@hotmail.com>
2019-06-14 17:50:26 +0530

    Updates

---
+++ precache-manifest.d4f0ed423e06bc7bb4dc7b02adbcf2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231c36018125cce26bc9c31db3c24f8b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "062f2090aad5ed0c3d02",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6da7067c091e0dc31d0b",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "062f2090aad5ed0c3d02",
+    "url": "/contactmanager/static/js/2.bcc2bc8d.chunk.js"
+  },
+  {
+    "revision": "6da7067c091e0dc31d0b",
+    "url": "/contactmanager/static/js/main.01652851.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
